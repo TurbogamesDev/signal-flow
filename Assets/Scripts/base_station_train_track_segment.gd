3337929ff2cb7501d8587a93b5352e3ecac30965
TurbogamesDev <turbogames.mail@gmail.com>
2026-02-10 08:52:39 +0530

    Debugging for godot v4.6

--- Assets/Scripts/base_station_train_track_segment.gd
+++ Assets/Scripts/base_station_train_track_segment.gd
@@ -1,12 +1,12 @@
 extends BaseTrainTrackSegment
 class_name BaseStationTrainTrackSegment
 
-@export var trainTrackSegment: BaseTrainTrackSegment
+@export var entryTrainTrackSegment: BaseTrainTrackSegment
 
 func inheritPropertiesOfTrainTrackSegment(train_track_segment: BaseTrainTrackSegment):
-    directionMap = train_track_segment.directionMap
-    entryDirectionToTrainTrackPieceMap = train_track_segment.entryDirectionToTrainTrackPieceMap
-    trainTrackPieces = train_track_segment.trainTrackPieces
+	directionMap = train_track_segment.directionMap
+	entryDirectionToTrainTrackPieceMap = train_track_segment.entryDirectionToTrainTrackPieceMap
+	trainTrackPieces = train_track_segment.trainTrackPieces
 
 func _ready() -> void:
-    inheritPropertiesOfTrainTrackSegment(trainTrackSegment)
+	inheritPropertiesOfTrainTrackSegment(entryTrainTrackSegment)
