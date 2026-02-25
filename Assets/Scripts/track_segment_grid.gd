47257d1233e399cb6b85001a486b3ec98db6557b
TurbogamesDev <turbogames.mail@gmail.com>
2026-02-25 09:16:07 +0530

    Added Enums for Track Segments

--- Assets/Scripts/track_segment_grid.gd
+++ Assets/Scripts/track_segment_grid.gd
@@ -1,12 +1,30 @@
 extends TileMapLayer
 class_name TrackSegmentGrid
 
-
-
-
-
-func placeSegment(segment_type: int, segment: int, segment_position: Vector2i):
+func placeSegment(segment_type: Enums.TrackSegmentType, segment: int, segment_position: Vector2i):
     self.set_cell(segment_position, segment_type, Vector2i(0, 0), segment)
 
 func _ready() -> void:
-    placeSegment(0, 0, Vector2i(0, 0))
+    placeSegment(
+        Enums.TrackSegmentType.REGULAR_TRACK_SEGMENT,
+        Enums.RegularTrackSegment.EAST_TO_WEST,
+        Vector2i(-1, -1)
+    )
+
+    placeSegment(
+        Enums.TrackSegmentType.SINGULAR_TRACK_SEGMENT, 
+        Enums.SingularTrackSegment.HORIZONTAL_FOLLOWING_TO_OPPOSING,
+        Vector2i(0, -1)
+    )
+
+    placeSegment(
+        Enums.TrackSegmentType.STATION_TRACK_SEGMENT,
+        Enums.StationTrackSegment.NORTH_ENTRY_TERMINATING,
+        Vector2i(-1, 0)
+    )
+
+    placeSegment(
+        Enums.TrackSegmentType.SWITCHING_TRACK_SEGMENT,
+        Enums.SwitchingTrackSegment.EAST_TO_WEST_AND_NORTH_TO_WEST,
+        Vector2i(0, 0)
+    )
